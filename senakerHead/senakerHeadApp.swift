fb961a0911a1f22c11919f0c20ca558ca1087e4d
Rupaj Sen <rupajps4@gmail.com>
2022-09-24 13:07:19 +0530

    Initial Commit

---
+++ senakerHead/senakerHeadApp.swift
@@ -0,0 +1,17 @@
+//
+//  senakerHeadApp.swift
+//  senakerHead
+//
+//  Created by Rupaj Sen on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct senakerHeadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
